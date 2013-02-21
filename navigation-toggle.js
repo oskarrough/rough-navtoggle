905908dd8caafe233674b7df791e81fdfaf54d7c
Oskar Rough <oskarrough@gmail.com>
2013-02-21 16:54:57 +0100

    Naming conventing

--- navigation-toggle.js
+++ navigation-toggle.js
@@ -3,13 +3,13 @@ $('.no-js').removeClass('no-js');
 $(function(){
 
 	// Add toggles
-	var $navsWithToggle = $('#js-menu');
+	var $navsWithToggle = $('#js-rough-navigation-toggle');
 	$navsWithToggle.each(function() {
-		$(this).before('<div class="nav-toggle"><span class="nav-toggle__text">Menu</span> <span class="nav-toggle__box"><i class="nav-toggle__icon"></i></span></div>');
+		$(this).before('<div class="navigation-toggle-button"><span class="navigation-toggle-button__text">Menu</span> <span class="navigation-toggle-button__box"><i class="navigation-toggle-button__icon"></i></span></div>');
 	});
 
 	// Do the toggle
-	$('.nav-toggle').click(function() {
+	$('.navigation-toggle-button').click(function() {
 		$(this).toggleClass('is-active');
 		$navsWithToggle.toggleClass('is-active');
 		return false;
